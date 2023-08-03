62625265156b20a3915a7d4b320b3247524463ce
Vyacheslav Kovalenko <vjacheslavkovalenko@gmail.com>
2023-08-03 19:36:33 +0300

    first commit with android without github

---
+++ app/src/main/java/by/neco_lesson1_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package by.neco_lesson1_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
